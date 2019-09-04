2242aceb4b452facfd475220e1c2eb20f02c4cad
Yaşar ÖZYURT <blueromans@hotmail.com>
2019-09-04 23:27:15 +0300

    Files page implementation.

--- app/views/activities.js
+++ app/views/activities.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Text} from 'native-base';
+import {Body, Button, Container, Content, Header, Left, Right, Text, Title} from 'native-base';
 import {Image, View} from 'react-native';
 
 
@@ -11,34 +11,41 @@ class ActivitiesPage extends Component {
 
     render() {
         return (
+            <Container style={{
+                backgroundColor: 'transparent',
+            }}>
+                <Content>
+                    <View style={{marginVertical:30}}>
+                        <View style={{flex: 3, paddingHorizontal: 30, justifyContent: 'center', alignItems: 'center'}}>
+                            <Image style={{resizeMode: 'cover'}} source={require('../assets/images/activities.png')}/>
+                            <Text style={{fontSize: 20, marginVertical: 20}}>Add Activities</Text>
+                            <Text style={{fontSize: 13, paddingHorizontal: 40, color: '#9B9B9B', textAlign: 'center'}}>Add your
+                                clients activities here and Aidy will keep track of all. You will no longer miss a single client
+                                task or forget an activity.</Text>
+                        </View>
+                        <View style={{flex:3,marginVertical:30}}>
+                            <View style={{borderBottomColor:'#D9D9D9',borderBottomWidth:.5,borderTopWidth:.5,borderTopColor:'#D9D9D9'}}>
+                                <Button transparent full>
+                                    <Text>Add Task</Text>
+                                </Button>
+                            </View>
+                            <View style={{borderBottomColor:'#D9D9D9',borderBottomWidth:.5}}>
+                                <Button transparent full>
+                                    <Text>Add Showing</Text>
+                                </Button>
+                            </View>
+                            <View style={{borderBottomColor:'#D9D9D9',borderBottomWidth:.5}}>
+                                <Button transparent full>
+                                    <Text>Add Offer</Text>
+                                </Button>
+                            </View>
+                        </View>
 
-            <View style={{flex:1}}>
-                <View style={{flex: 3, paddingHorizontal: 30, justifyContent: 'center', alignItems: 'center'}}>
-                    <Image style={{resizeMode: 'cover'}} source={require('../assets/images/activities.png')}/>
-                    <Text style={{fontSize: 20, marginVertical: 20}}>Add Activities</Text>
-                    <Text style={{fontSize: 13, paddingHorizontal: 40, color: '#9B9B9B', textAlign: 'center'}}>Add your
-                        clients activities here and Aidy will keep track of all. You will no longer miss a single client
-                        task or forget an activity.</Text>
-                </View>
-                <View style={{flex:3}}>
-                    <View style={{borderBottomColor:'#D9D9D9',borderBottomWidth:.5,borderTopWidth:.5,borderTopColor:'#D9D9D9'}}>
-                        <Button transparent full>
-                            <Text>Add Task</Text>
-                        </Button>
                     </View>
-                    <View style={{borderBottomColor:'#D9D9D9',borderBottomWidth:.5}}>
-                        <Button transparent full>
-                            <Text>Add Showing</Text>
-                        </Button>
-                    </View>
-                    <View style={{borderBottomColor:'#D9D9D9',borderBottomWidth:.5}}>
-                        <Button transparent full>
-                            <Text>Add Offer</Text>
-                        </Button>
-                    </View>
-                </View>
 
-            </View>
+                </Content>
+            </Container>
+
 
         );
     }
--- app/views/files.js
+++ app/views/files.js
@@ -1,22 +1,107 @@
 import React, {Component} from 'react';
-import {Container, Content,Text} from 'native-base';
+import {Body, Button, Container, Content, Left, ListItem, Right, Text} from 'native-base';
+import {FlatList, Image, View} from 'react-native';
 
-
+const EmptyComponent = ({ title }) => (
+    <View style={styles.emptyContainer}>
+        <Text style={styles.emptyText}>{title}</Text>
+    </View>
+);
+const styles={
+    emptyContainer: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    emptyText: {
+        color:'#9B9B9B',
+        marginVertical:20,
+        fontSize: 14,
+    },
+}
 class FilesPage extends Component {
-
+    state ={
+        files: [
+            {
+                date: 'Dec. 31th - Friday',
+                files:[
+                    {
+                        image: require('../assets/images/yer_gosterme.png'),
+                        title: 'Yer gösterme belgesi - Ev id : 215'
+                    }
+                ]
+            },
+            {
+                date: 'Dec. 28th - Wednesday',
+                files:[
+                    {
+                        image: require('../assets/images/kimlik.png'),
+                        title: 'Kimlik belgesi'
+                    },
+                    {
+                        image: require('../assets/images/yer_gosterme.png'),
+                        title: 'Yer gösterme belgesi - Ev id : 78'
+                    }
+                ]
+            },
+        ],
+    }
     constructor(props) {
         super(props);
     }
 
     render() {
+        const {files} = this.state;
         return (
 
             <Container style={{
                 backgroundColor: 'transparent',
             }}>
-                <Content padder>
-                    <Text>Files</Text>
+                <Content>
+                        <View style={{flex:1}}>
+                            <View style={{flex:1,paddingHorizontal:20,alignItems: 'flex-end'}}>
+                                <Button transparent>
+                                    <Text>Add File</Text>
+                                </Button>
+                            </View>
+                            <View style={{flex:1}}>
+                                <FlatList
+                                    keyExtractor={(item, index) => index.toString()}
+                                    data={files}
+                                    ListEmptyComponent={
+                                        <EmptyComponent title="Nothing here, come back later.." />
+                                    }
+                                    renderItem={({ item }) => (
+                                        <View style={{flex:1,borderBottomWidth:1,borderBottomColor:'#B2B2B2'}}>
+                                            <Text style={{paddingHorizontal:20,paddingVertical:5}}>{item.date}</Text>
+                                            {item.files.map((file, index) => {
+                                                return (
+                                                    <ListItem>
+                                                        <Left style={{flex:1}}>
+                                                            <Image style={{height:70,resizeMode:'contain'}} source={file.image} />
+                                                        </Left>
+                                                        <Body style={{flex:5}}>
+                                                            <View style={{flex:1,marginBottom:10,justifyContent: 'flex-start'}}>
+                                                                <Text>{file.title}</Text>
+                                                            </View>
+                                                            <View style={{flex:1,alignItems:'flex-end',marginTop:5}}>
+                                                                <Button transparent>
+                                                                    <Text>delete</Text>
+                                                                </Button>
+                                                            </View>
+                                                        </Body>
+                                                        <Right style={{flex:0}}/>
+                                                    </ListItem>
+                                                );
+                                            })}
+                                        </View>
 
+
+
+                                    )}
+                                />
+                            </View>
+                        </View>
                 </Content>
             </Container>
         );
